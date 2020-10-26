3fe239260d924fc2cc15cfa7ab6509a166ae2edf
klimarbeit <56062404+klimarbeit@users.noreply.github.com>
2020-10-26 18:35:23 +0300

    Create kerio_sync_conf.py

---
+++ kerio_sync_conf.py
@@ -0,0 +1,8 @@
+username = "user"
+password = "password"
+
+# masterserver (source)
+masterserver = "https://xxx.xxx.xxx.xxx:4081"
+
+# slaveserver(s) (destination)
+slaveservers = ["https://xxx.xxx.xxx.xxx:4081"]
